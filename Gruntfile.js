eb83812245383e2c8f59eef4240f504986052b6b
Nicolas DUBIEN <github@dubien.org>
2015-04-23 21:52:39 +0200

    Adding test-coverage Inspired from https://github.com/thorst/Code-Coverage-Qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,34 @@ module.exports = function(grunt) {
 	//Project configuration.
 	grunt.initConfig({
 		qunit: {
+			options: {
+				timeout: 30000,
+				"--web-security": "no",
+				coverage: {
+					src: ["js/*.js" ],
+					instrumentedFiles: "temp/",
+					htmlReport: "build/report/coverage",
+					lcovReport: "build/report/lcov",
+					linesThresholdPct: 0
+				}
+			},
 			files: ['test/*.html']
+		},
+		coveralls: {
+			options: {
+				force: true
+			},
+			main_target: {
+				src: "build/report/lcov/lcov.info"
+			}
 		}
 	});
 
 	// Load plugin
-	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks("grunt-coveralls");
+	grunt.loadNpmTasks("grunt-qunit-istanbul");
 
 	// Task to run tests
-	grunt.registerTask('test', 'qunit');
+	grunt.registerTask('default', 'qunit');
 };
 
